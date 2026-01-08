f2f1848275e953c70afc13855cf2ed7413eb93dd
diego <240680@utxicotepec.edu.mx>
2026-01-08 14:28:51 -0600

    crear funciones js y estilo del encabezado

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
 function saludar(){
     alert("Bienvenido a la academia de T.I");
 }
+function suma(){
+    var valor1 =24;
+    var valor2 =13;
+    var resultado = valor1 + valor2;
+    alert("resultado de la suma es:" +resultado);
+}
+function resta(){
+    var valor1 =24;
+    var valor2 =13;
+    var resultado = valor1 - valor2;
+    alert("resultado de la resta de" +valor1+" y " +valor2+" es: " +resultado);
+}
+function multiplicar(){
+    var valor1 =24;
+    var valor2 =13;
+    var resultado = valor1 * valor2;
+     alert("resultado de la multiplicacion de" +valor1+" y " +valor2+" es: " +resultado);
+}
+function dividir(){
+    var valor1 =24;
+    var valor2 =13;
+    var resultado = valor1 / valor2;
+     alert("resultado de la divicion de" +valor1+" y " +valor2+" es: " +resultado);
+}
